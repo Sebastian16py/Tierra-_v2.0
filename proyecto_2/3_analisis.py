6d8a2e0532f7651f34bc6ab26ef3047ec6eb4aca
Sebastian16py <sebamore.398@gmail.com>
2026-04-21 01:56:32 -0500

    Segundo Query y Filtro realizado correctamente

--- proyecto_2/3_analisis.py
+++ proyecto_2/3_analisis.py
@@ -13,6 +13,13 @@ conn = sqlite3.connect('sistemas_planetarios.db')
 QUERY = 'SELECT AVG(pl_rade) AS Radio_promedio, discoverymethod AS Metodo FROM planetas GROUP BY discoverymethod'
 df = pd.read_sql_query(QUERY, conn)
 
-conn.close()
 
+# Se muestran por consolas el promedio de radio para cada grupo de exoplaneta
 print(df)
+
+# Segunda Consulta: filtro de habitabilidad
+QUERY_2 = 'SELECT pl_eqt AS Temperatura_equilibrio, pl_rade AS Radio_promedio FROM planetas WHERE 200<pl_eqt<320 AND pl_rade<2.5'
+df_2 = pd.read_sql_query(QUERY_2, conn)
+print('Filtro de habitabilidad realizado')
+conn.close()
+ 
