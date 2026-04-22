3187cff15206318748de0fe00878daa565a1cff6
Sebastian16py <sebamore.398@gmail.com>
2026-04-22 14:39:34 -0500

    Programa de analisis y grafica completado

--- proyecto_2/3_analisis.py
+++ proyecto_2/3_analisis.py
@@ -3,7 +3,6 @@ import pandas as pd
 import sqlite3
 import numpy as np
 import matplotlib.pyplot as plt
-import seaborn as sns
 
 
 # Conectar con la base de datos
@@ -13,13 +12,31 @@ conn = sqlite3.connect('sistemas_planetarios.db')
 QUERY = 'SELECT AVG(pl_rade) AS Radio_promedio, discoverymethod AS Metodo FROM planetas GROUP BY discoverymethod'
 df = pd.read_sql_query(QUERY, conn)
 
-
-# Se muestran por consolas el promedio de radio para cada grupo de exoplaneta
+# Se muestran por consola el promedio de radio para cada grupo de exoplaneta
 print(df)
 
 # Segunda Consulta: filtro de habitabilidad
-QUERY_2 = 'SELECT pl_eqt AS Temperatura_equilibrio, pl_rade AS Radio_promedio FROM planetas WHERE 200<pl_eqt<320 AND pl_rade<2.5'
+QUERY_2 = 'SELECT pl_eqt AS Temperatura_equilibrio, pl_rade AS Radio_promedio, st_teff  FROM planetas WHERE pl_eqt BETWEEN 200 AND 320 AND pl_rade<2.5'
 df_2 = pd.read_sql_query(QUERY_2, conn)
 print('Filtro de habitabilidad realizado')
+
+# DaAtos generales
+QUERY_3 = 'SELECT st_teff, pl_eqt FROM planetas'
+df_3 = pd.read_sql_query(QUERY_3,conn)
 conn.close()
- 
+
+
+# Grafica 
+plt.style.use('dark_background')
+plt.scatter(df_3['st_teff'], df_3['pl_eqt'], c='gray', label = 'Exoplanetas detectados en La Silla')
+plt.scatter(df_2['st_teff'], df_2['Temperatura_equilibrio'], c='darkblue', label='Potencialmente habitables')
+
+plt.grid(alpha=0.3)
+plt.xlabel('Temporatura de la Estrella')
+plt.ylabel('Temperatura del exoplaneta')
+plt.legend()
+plt.show(block=False) 
+plt.savefig('filtro_habitabilidad.png')
+print('Imagen generada exitosamente')
+
+
